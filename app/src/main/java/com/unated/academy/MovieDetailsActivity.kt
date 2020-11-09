47df6552895eb8969d79387c0d2a8e205208c652
Dmitry Abramichev <abramichev@devpnd.com>
2020-11-10 01:39:47 +0300

    + Movie details activity (LinearLayout / RelativeLayout combination)

---
+++ app/src/main/java/com/unated/academy/MovieDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.unated.academy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MovieDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+
+}
