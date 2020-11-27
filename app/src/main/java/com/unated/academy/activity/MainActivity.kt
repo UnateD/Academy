547b4e19ac7efa7e9aec20c7da16468a96a9e5ff
Dmitry Abramichev <abramichev@devpnd.com>
2020-11-27 18:37:49 +0300

    + Homework 3: Movies list + navigation between fragments

---
+++ app/src/main/java/com/unated/academy/activity/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.unated.academy.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.unated.academy.NavigationListener
+import com.unated.academy.R
+import com.unated.academy.addFragment
+import com.unated.academy.fragment.FragmentMoviesDetails
+import com.unated.academy.fragment.FragmentMoviesList
+import com.unated.academy.replaceFragment
+
+class MainActivity : AppCompatActivity(), NavigationListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if(savedInstanceState == null) {
+            addFragment(R.id.container, FragmentMoviesList())
+        }
+    }
+
+    override fun goToDetails() {
+        replaceFragment(R.id.container, FragmentMoviesDetails())
+    }
+}
